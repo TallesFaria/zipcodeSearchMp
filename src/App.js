53e32356dfdd1aaa198de9903ed3d9c83afc6af9
TallesFaria <tallesbeginsc@gmail.com>
2017-03-02 21:23:36 -0300

    Sent version

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ import { Map, Places } from './components'
 
 
 class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            search: ''
+        }
+    }
+
+    submitSearch(event) {
+        console.log('Submit Search:' + event.target.value)
+    }
+
+    updateSearch(event) {
+        console.log('Change Search:' + event.target.value)
+        let updateSearch = Object.assign({}, this.state.search)
+        updateSearch['search'] = event.targer.value
+        this.setState({
+            search: updateSearch
+        })
+    }
+
     render() {
 
         const location = {
@@ -21,25 +42,44 @@ class App extends Component {
             }
         ]
 
+        const search = ''
+
         return (
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h3>Zipcode Search</h3>
-                    
+
 
 
                 </div>
-                <div>
-                    <input type="text" placeholder="Type a zipcode here"/><br/>
-                </div>                
 
-                
-                <div style={{ width: 1250, height: 430, background: 'red' }}>
-                    <Map center={location } markers = {markers}  />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12">
+
+                            <form className="form-inline">
+                                <div className="row">
+                                    <div className="col-xs-8 col-sm-10">
+
+                                        <div className="form-group">
+                                            <input onChange={this.updateSearch.bind(this)} type="text"
+                                                className="form-control input-lg"
+                                                placeholder="Type a zipcode here"
+                                                required />
+                                            <button onClick={this.submitSearch.bind(this)}>Search</button><br />
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
 
-                <Places />
+
+                <div style={{ width: 1285, height: 430, background: 'red' }}>
+                    <Map center={location} markers={markers} search = {search}/>
+                </div>
             </div>
         );
     }
--- src/components/Map.js
+++ src/components/Map.js
@@ -3,6 +3,30 @@ import { GoogleMapLoader, GoogleMap, Marker, google } from 'react-google-maps'
 
 export default class Map extends Component {
 
+    // geocodeAddress(address) {
+    //     this.geocoder.geocode({ 'address': address }, function handleResults(results, status) {
+
+    //         if (status === google.maps.GeocoderStatus.OK) {
+
+    //             this.map.setCenter(results[0].geometry.location);
+    //             this.markers.setPosition(results[0].geometry.location);
+
+    //             return;
+    //         }
+
+    //         this.map.setCenter({
+    //             lat: ATLANTIC_OCEAN.latitude,
+    //             lng: ATLANTIC_OCEAN.longitude
+    //         });
+
+    //         this.markers.setPosition({
+    //             lat: ATLANTIC_OCEAN.latitude,
+    //             lng: ATLANTIC_OCEAN.longitude
+    //         });
+
+    //     }.bind(this));
+    // }
+
 
     render() {
         const mapContainer = <div style={{ height: '100%', width: '100%' }}></div>
@@ -20,8 +44,16 @@ export default class Map extends Component {
         })
 
 
+        // const search = this.props.search
+        // this.geocoder = new google.maps.Geocoder();
+
+        // address = this.props.center
+        
+        // this.geocodeAddress(address)
+
+
         return (
-            
+
             <GoogleMapLoader
                 containerElement={mapContainer}
 
